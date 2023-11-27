d7bd2a1712410ea2c5044e57f520f206b5d9e97a
kimmykokonut <kimmykokonut@gmail.com>
2023-11-27 09:51:34 -0800

    pass tests quarter-count
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- __tests__/quarter-count.test.js
+++ __tests__/quarter-count.test.js
@@ -1,7 +1,12 @@
-import quarterCount from '../src/js/quarter-count';
+import { quarterCount } from '../src/js/quarter-count.js';
 
 describe('quarterCount()', () => {
+
   test('should return number of quarters in 25 cents', () => {
     expect(quarterCount(25)).toEqual(1);
   });
+  test('should return undefined if NaN input', () => {
+    expect(quarterCount(NaN)).toEqual(undefined);
+  });
+
 });
--- src/js/quarter-count.js
+++ src/js/quarter-count.js
@@ -1,3 +1,11 @@
-export default function quarterCount() {
-    
+export const quarterCount = (value) => {
+  if (isNaN(value)) {
+    return;
+  }
+  if (value <= 24) {
+    return 0;
+  } else {
+    value = value - 25;
+    return quarterCount(value) + 1;
+  }
 }
