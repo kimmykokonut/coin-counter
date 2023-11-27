cf4f459690879b5ccd7339b94bd4a85c04eda517
kimmykokonut <kimmykokonut@gmail.com>
2023-11-27 10:10:55 -0800

    pass test 1 remainder()
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- __tests__/quarter-count.test.js
+++ __tests__/quarter-count.test.js
@@ -1,12 +1,18 @@
-import { quarterCount } from '../src/js/quarter-count.js';
+import { experiments } from 'webpack';
+import { quarterCount, remainder } from '../src/js/quarter-count.js';
 
 describe('quarterCount()', () => {
-
   test('should return number of quarters in 25 cents', () => {
     expect(quarterCount(25)).toEqual(1);
   });
   test('should return undefined if NaN input', () => {
     expect(quarterCount(NaN)).toEqual(undefined);
+  });
+
+  describe('remainder()', () => {
+    test('should return remainder of number input % 25', () => {
+      expect(remainder(55)).toEqual("less than 9 cents left. 5");
+    });
   });
 
 });
--- src/js/quarter-count.js
+++ src/js/quarter-count.js
@@ -9,3 +9,13 @@ export const quarterCount = (value) => {
     return quarterCount(value) + 1;
   }
 }
+export const remainder = (value) => {
+  const remValue = value % 25;
+  if (remValue === 0) {
+    return 0;
+  } else if (remValue > 9) {
+    return `the remainder is ${remValue}`; //dimeCount(remainder)
+  } else {
+    return `less than 9 cents left. ${remValue}`;
+  }
+}
